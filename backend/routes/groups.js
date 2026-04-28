7cc9d6c85e23e6bad28e42edac77bde5e29073e4
bhavyareddy1807-stack <bhavyareddy1807@gmail.com>
2026-04-28 20:18:41 +0530

    Add group routes for CRUD operations

--- backend/routes/groups.js
+++ backend/routes/groups.js
@@ -1,1 +1,21 @@
+const express = require('express');
+const router = express.Router();
+const {
+  createGroup,
+  getGroups,
+  getGroupById,
+  addMemberToGroup
+} = require('../controllers/groupController');
 
+// All routes here are prefixed with /api/groups
+router.route('/')
+  .get(getGroups)
+  .post(createGroup);
+
+router.route('/:id')
+  .get(getGroupById);
+
+router.route('/:id/members')
+  .post(addMemberToGroup);
+
+module.exports = router;
